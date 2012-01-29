67ebb90784141cbc9f9caa391cd36f415b441382
Josh Fradley <josh.fradley@gmail.com>
2012-01-29 11:07:21 +0000

    Dont bother connecting to databse before we have an ID, allow POST requests to get.php

--- get.php
+++ get.php
@@ -11,17 +11,12 @@ ob_start();
 <body>
 <?php
 
-//Connect to database
-require_once("config.php");
-
-$con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-if (!$con) {
-    die("Could not connect: " . mysql_error());
+//Accept POST or GET
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+} else {
+    $id = $_POST["id"];
 }
-
-mysql_select_db(DB_NAME, $con);
-
-$id = mysql_real_escape_string($_GET["id"]);
 
 //Check ID is not blank
 if (empty($id)) {
@@ -32,6 +27,16 @@ if (empty($id)) {
 if (!preg_match("/^[a-zA-Z0-9.]{1,}$/", $id)) {
     die("<h1>SHTracker: Error</h1><p>Please enter only numbers, letters or points.</p><hr /><p><a href=\"javascript:history.go(-1)\">Go Back</a></p></body></html>"); 
 }
+
+//Connect to database
+require_once("config.php");
+
+$con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+if (!$con) {
+    die("Could not connect: " . mysql_error());
+}
+
+mysql_select_db(DB_NAME, $con);
 
 //If ID exists update count or die
 $getinfo = "SELECT * FROM Data WHERE id = \"$id\"";
