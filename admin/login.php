53fa0b488a826caab75126293f23c25069f6035d
Josh Fradley <josh.fradley@gmail.com>
2012-02-26 18:49:53 +0000

    Add remember me feature to login

--- admin/login.php
+++ admin/login.php
@@ -13,9 +13,17 @@ $user = ADMIN_USER;
 
 session_start();
 
+//If cookie is set, skip login
+if (isset($_COOKIE["shtrackerrememberme"])) {
+    $_SESSION["is_logged_in"] = true;
+}
+
 if (isset($_POST["password"]) && isset($_POST["user"])) {
     if (sha1($_POST["password"]) == $password && $_POST["user"] == $user) {
         $_SESSION["is_logged_in"] = true;
+            if (isset($_POST["rememberme"])) {
+                setcookie("shtrackerrememberme", ADMIN_USER, time()+1209600);
+            }
     } else {
         die("<html><head><title>SHTracker: Login</title><link rel=\"stylesheet\" type=\"text/css\" href=\"../style.css\" /></head><body><h1>SHTracker: Error</h1><p>Incorrect password or username...</p><hr /><p><a href=\"../admin\">&larr; Go Back</a></p></body></html>");
     }
@@ -35,6 +43,7 @@ if(!isset($_SESSION["is_logged_in"])) {
 <form method="post">
 User: <input type="text" name="user" /><br />
 Password: <input type="password" name="password" />
+<p><input type="checkbox" name="rememberme">Keep me logged in</p>
 <p><input type="submit" name="submit" value="Login" /></p>
 </form>
 <p><a href="http://<?php echo $_SERVER["HTTP_HOST"]; ?>">&larr; Back to <?php echo WEBSITE; ?></a></p>
--- admin/logout.php
+++ admin/logout.php
@@ -5,6 +5,10 @@
 session_start();
 unset($_SESSION["is_logged_in"]);
 session_destroy();
+if (isset($_COOKIE["shtrackerrememberme"])) {
+	setcookie("shtrackerrememberme", "", time()-86400);
+}
 header("Location: login.php");
+exit;
 
 ?>
