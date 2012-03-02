89499e11d589cf1477053806d98598420d96873f
Josh Fradley <josh.fradley@gmail.com>
2012-03-02 10:08:45 +0000

    Alert on mysql error

--- admin/index.php
+++ admin/index.php
@@ -29,6 +29,9 @@ if (!$con) {
 mysql_select_db(DB_NAME, $con);
 
 $getdownloads = mysql_query("SELECT * FROM Data ORDER BY name ASC");
+if (!$getdownloads) {
+    die("<p>Invalid query: " . mysql_error()) . "</p>";
+}
 
 echo "<h1>SHTracker: " . WEBSITE . " Download Statistics</h1>
 <form action=\"manage.php\" method=\"post\"><table>
