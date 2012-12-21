7f23fbaace6e771e4f333ee809b4e09571cc8fd6
Josh Fradley <josh.fradley@gmail.com>
2012-12-21 18:54:45 +0000

    Update settings

--- admin/settings.php
+++ admin/settings.php
@@ -128,8 +128,9 @@ header("Location: settings.php?updated=true");
 <li class="divider-vertical"></li>
 <li><a href="add.php">Add</a></li>
 <li><a href="#">Edit</a></li>
-<li class="divider-vertical"></li>
-<li class="active"><a href="settings.php">Settings</a></li>
+</ul>
+<ul class="nav pull-right">
+<li><a href="settings.php">Settings</a></li>
 <li><a href="logout.php">Logout</a></li>
 </ul>
 </div>
@@ -151,74 +152,77 @@ if (isset($_GET["updated"])) {
 ?>
 <form method="post">
 <fieldset>
-<legend>Admin Details</legend>
-<label for="adminuser">Admin User</label>
-<input type="text" id="adminuser" name="adminuser" value="<? echo $currentadminuser; ?>">
-<label for="adminpassword">Admin Password</label>
-<input type="password" id="adminpassword" name="adminpassword" value="<? echo $currentadminpassword; ?>">
-<legend>Other Settings</legend>
-<label for="website">Website</label>
-<input type="text" id="website" name="website" value="<? echo $currentwebsite; ?>">
-<label for="pathtoscript">Path to Script</label>
-<input type="text" id="pathtoscript" name="pathtoscript" value="<? echo $currentpathtoscript; ?>">
-<legend>Ad Code</legend>
-<span class="help-block">Show an advert before user can continue to their download. This can be changed on a per download basis.</span>
-<div class="alert alert-info"><b>N.B:</b> On some server configurations using html code here may produce errors.</div>
-<textarea name="adcode"><? echo $currentadcode; ?></textarea>
-<legend>Count Unique Visitors Only:</legend>
-<span class="help-block">This settings allows you to make sure an individual users' clicks are only counted once.</span>
+<h4>Admin Details</h4>
+
+<div class="control-group">
+<label class="control-label" for="adminuser">Admin User</label>
+<div class="controls">
+<input type="text" id="adminuser" name="adminuser" value="<? echo $currentadminuser; ?>" placeholder="Enter a admin user...">
+</div>
+</div>
+
+<div class="control-group">
+<label class="control-label" for="adminpassword">Admin Password</label>
+<div class="controls">
+<input type="password" id="adminpassword" name="adminpassword" value="<? echo $currentadminpassword; ?>" placeholder="Enter a admin password...">
+</div>
+</div>
+
+<h4>Other Settings</h4>
+
+<div class="control-group">
+<label class="control-label" for="website">Website</label>
+<div class="controls">
+<input type="text" id="website" name="website" value="<? echo $currentwebsite; ?>" placeholder="Enter your websites name...">
+</div>
+</div>
+
+<div class="control-group">
+<label class="control-label" for="pathtoscript">Path to Script</label>
+<div class="controls">
+<input type="text" id="pathtoscript" name="pathtoscript" value="<? echo $currentpathtoscript; ?>" placeholder="Type where SHTracker is installed...">
+</div>
+</div>
+
+<h4>Ad Code</h4>
+
+<p>Show an advert before user can continue to their download. This can be changed on a per download basis.</p>
+<div class="alert alert-warning"><b>Warning:</b> On some server configurations using HTML code here may produce errors.</div>
+
+<div class="control-group">
+<div class="controls">
+<textarea name="adcode" placeholder="Enter a ad code..."><? echo $currentadcode; ?></textarea>
+</div>
+</div>
+
+<h4>Count Unique Visitors Only</h4>
+<p>This settings allows you to make sure an individual users' clicks are only counted once.</p>
+<div class="control-group">
+<label class="control-label">Enable/Disable</label>
+<div class="controls">
 <?php
 if ($currentcountuniqueonlystate == "Enabled" ) {
-    echo "<label for=\"countuniqueonlytime\">Time to consider user unique</label>
-    <input type=\"text\" id=\"countuniqueonlytime\" name=\"countuniqueonlytime\" value=\"$currentcountuniqueonlytime\">
-    <p><input type=\"radio\" id=\"countuniqueonlystate\" name=\"countuniqueonlystate\" value=\"Enabled\" checked=\"checked\"> Enabled<br />
-    <input type=\"radio\" id=\"countuniqueonlystate\" name=\"countuniqueonlystate\" value=\"Disabled\"> Disabled</p>";
+    echo "<label class=\"radio\"><input type=\"radio\" name=\"countuniqueonlystate\" value=\"Enabled\" checked=\"checked\"> Enabled</label>
+          <label class=\"radio\"><input type=\"radio\" name=\"countuniqueonlystate\" value=\"Disabled\"> Disabled</label>";    
 } else {
-    echo "<p><input type=\"radio\" id=\"countuniqueonlystate\" name=\"countuniqueonlystate\" value=\"Enabled\"> Enabled<br />
-    <input type=\"radio\" id=\"countuniqueonlystate\" name=\"countuniqueonlystate\" value=\"Disabled\" checked=\"checked\"> Disabled</p>";
-}
-?>
-<p><button type="submit" name="save" class="btn btn-primary">Save Changes</button></p>
+  echo "<label class=\"radio\"><input type=\"radio\" name=\"countuniqueonlystate\" value=\"Enabled\"> Enabled</label>
+    <label class=\"radio\"><input type=\"radio\" name=\"countuniqueonlystate\" value=\"Disabled\" checked=\"checked\"> Disabled</label>";   
+}   
+?> 
+</div>  
+</div>
+<div class="control-group">
+<label class="textarea" for="countuniqueonlytime">Time to consider user unique</label>
+<div class="controls">
+<input type="text" id="countuniqueonlytime" name="countuniqueonlytime" value="<? echo $currentcountuniqueonlytime; ?>" placeholder="Enter a time...">
+</div>
+</div>
+<div class="form-actions">
+<button type="submit" name="save" class="btn btn-primary">Save Changes</button>
+</div>
 </fieldset>
 </form>
-<h3>Database Backup</h3>
-<?
-
-if (isset($_GET["backup"])) {
-
-    //Connect to database    
-    $con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-    if (!$con) {
-        die("Could not connect: " . mysql_error());
-    }
-    
-    mysql_select_db(DB_NAME, $con);
-    
-    $getdata = mysql_query("SELECT * FROM Data");
-    $string = "CREATE TABLE Data (
-    name VARCHAR(100) NOT NULL,
-    id VARCHAR(25) NOT NULL,
-    url VARCHAR(200) NOT NULL,
-    count INT(10) NOT NULL default \"0\",
-    protect TINYINT(1) NOT NULL default \"0\",
-    password VARCHAR(200),
-    showads TINYINT(1) NOT NULL default \"0\",
-    PRIMARY KEY (id)
-    ) ENGINE = MYISAM; \n\nINSERT INTO Data (name, id, url, count, protect, password, showads) VALUES ";
-    
-    while($row = mysql_fetch_assoc($getdata)) {
-        $string .= "('" . $row["name"] . "', '" . $row["id"] . "', '" . $row["url"] . "', '" . $row["count"] . "', '" . $row["protect"] . "', '" . $row["password"] . "', '" . $row["showads"] . "'), ";
-    }
-    
-    //Remove last comma
-    $datastring = substr_replace($string, "", -2);
-    
-    echo "<p>Copy this somewhere safe and use your database admin tool to run as a SQL query.</p><p><textarea cols=\"80\" rows=\"16\">$datastring</textarea></p>"; 
-} else {
-    echo "<p><button class=\"btn\" onClick=\"window.location = 'settings.php?backup'\">Backup Database</button></p>";
-}
-
-?>
+</form>
 </div>
 <!-- Content end -->
 <!-- Javascript start -->	
