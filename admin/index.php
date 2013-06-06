38b151412190ef005122c300f9f99aec47a950fb
Josh Fradley <josh.fradley@gmail.com>
2013-06-06 15:55:08 +0100

    Back to work

--- admin/index.php
+++ admin/index.php
@@ -2,7 +2,7 @@
 
 //Indication, Copyright Josh Fradley (http://github.com/joshf/Indication)
 
-$version = "4.3";
+$version = "4.3.1dev";
 
 if (!file_exists("../config.php")) {
     header("Location: ../installer");
