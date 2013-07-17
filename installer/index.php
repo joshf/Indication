5c49e2d21f6727e56d5ebff9ea33c4e0ae4947ab
Josh Fradley <josh.fradley@gmail.com>
2013-07-17 14:04:35 +0100

    Update installer/add upgrade script

--- installer/index.php
+++ installer/index.php
@@ -2,12 +2,6 @@
 
 //Indication, Copyright Josh Fradley (http://github.com/joshf/Indication)
 
-//Security check, check if config exists
-if (file_exists("../config.php")) {
-    header("Location: ../admin");
-    exit;
-}
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -54,12 +48,79 @@ $(document).ready(function() {
 <h1>Installer</h1>
 </div>
 <?php
+
+//Check if Indication has been installed
+if (file_exists("../config.php")) {
+    die("<div class=\"alert alert-info\"><h4 class=\"alert-heading\">Information</h4><p>Indication has already been installed! To reinstall the app please delete your config file and run this installer again.</p><p><a class=\"btn btn-info\" href=\"javascript:history.go(-1)\">Go Back</a></p></div></div></body></html>");
+}
+
+if (isset($_POST["doinstall"])) {
+
+    $dbhost = $_POST["dbhost"];
+    $dbuser = $_POST["dbuser"];
+    $dbpassword = $_POST["dbpassword"];
+    $dbname = $_POST["dbname"];
+    $adminuser = $_POST["adminuser"];
+    if (empty($_POST["adminpassword"])) {
+        die("<div class=\"alert alert-error\"><h4 class=\"alert-heading\">Install Failed</h4><p>Error: No admin password set.</p><p><a class=\"btn btn-danger\" href=\"javascript:history.go(-1)\">Go Back</a></p></div></div></body></html>");
+    } else {
+        //Salt and hash passwords
+        $randsalt = md5(uniqid(rand(), true));
+        $salt = substr($randsalt, 0, 3);
+        $hashedpassword = hash("sha256", $_POST["adminpassword"]);
+        $adminpassword = hash("sha256", $salt . $hashedpassword);
+    }
+    $uniquekey = md5(microtime().rand());
+	$website = $_POST["website"];
+	$pathtoscript = $_POST["pathtoscript"];
+    $version = "4.4";
+    
+    $installstring = "<?php\n\n//Database Settings\ndefine('DB_HOST', " . var_export($dbhost, true) . ");\ndefine('DB_USER', " . var_export($dbuser, true) . ");\ndefine('DB_PASSWORD', " . var_export($dbpassword, true) . ");\ndefine('DB_NAME', " . var_export($dbname, true) . ");\n\n//Admin Details\ndefine('ADMIN_USER', " . var_export($adminuser, true) . ");\ndefine('ADMIN_PASSWORD', " . var_export($adminpassword, true) . ");\ndefine('SALT', " . var_export($salt, true) . ");\n\n//Other Settings\ndefine('UNIQUE_KEY', " . var_export($uniquekey, true) . ");\ndefine('WEBSITE', " . var_export($website, true) . ");\ndefine('PATH_TO_SCRIPT', " . var_export($pathtoscript, true) . ");\ndefine('AD_CODE', 'Ad code here...');\ndefine('COUNT_UNIQUE_ONLY_STATE', 'Enabled');\ndefine('COUNT_UNIQUE_ONLY_TIME', '24');\ndefine('IGNORE_ADMIN_STATE', 'Disabled');\ndefine('THEME', 'default');\ndefine('VERSION', " . var_export($version, true) . ");\n\n?>";
+	
+    //Check if we can connect
+    $con = mysql_connect($dbhost, $dbuser, $dbpassword);
+    if (!$con) {
+        die("<div class=\"alert alert-error\"><h4 class=\"alert-heading\">Install Failed</h4><p>Error: Could not connect to database (" . mysql_error() . "). Check your database settings are correct.</p><p><a class=\"btn btn-danger\" href=\"javascript:history.go(-1)\">Go Back</a></p></div></div></body></html>");
+    }
+
+    //Check if database exists
+    $does_db_exist = mysql_select_db($dbname, $con);
+    if (!$does_db_exist) {
+        die("<div class=\"alert alert-error\"><h4 class=\"alert-heading\">Install Failed</h4><p>Error: Database does not exist (" . mysql_error() . "). Check your database settings are correct.</p><p><a class=\"btn btn-danger\" href=\"javascript:history.go(-1)\">Go Back</a></p></div></div></body></html>");
+    }
+
+	//Create Data table
+	$createtable = "CREATE TABLE Data (
+	name VARCHAR(100) NOT NULL,
+	id VARCHAR(25) NOT NULL,
+	url VARCHAR(200) NOT NULL,
+	count INT(10) NOT NULL default \"0\",
+	protect TINYINT(1) NOT NULL default \"0\",
+	password VARCHAR(200),
+	showads TINYINT(1) NOT NULL default \"0\",
+	PRIMARY KEY (id)
+	) ENGINE = MYISAM;";
+
+	//Run query
+	mysql_query($createtable);
+
+	//Write Config
+	$configfile = fopen("../config.php", "w");
+	fwrite($configfile, $installstring);
+	fclose($configfile);
+
+	mysql_close($con);
+    
+    die("<div class=\"alert alert-success\"><h4 class=\"alert-heading\">Install Complete</h4><p>Indication has been successfully installed. Please delete the \"installer\" folder from your server, as it poses a potential security risk!</p><p>Your login details are shown below, please make a note of them.</p><ul><li>User: $adminuser</li><li>Password: <i>Password you set during install</i></li></ul><p><a href=\"../admin/login.php\" class=\"btn btn-success\">Go To Login</a></p></div></div></body></html>");
+}
+
 //Get path to script
 $currenturl = $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
 $pathtoscriptwithslash = "http://" . substr($currenturl, 0, strpos($currenturl, "installer"));
-$pathtoscript = rtrim($pathtoscriptwithslash, "/");
+$pathtoscript = rtrim($pathtoscriptwithslash, "/");	
+
 ?>	
-<form action="install.php" method="post">
+<form method="post">
 <fieldset>
 <h4>Database Settings</h4>
 <div class="control-group">
---
+++ installer/upgrade.php
@@ -0,0 +1,138 @@
+<?php
+
+//Indication, Copyright Josh Fradley (http://github.com/joshf/Indication)
+
+if (!file_exists("../config.php")) {
+    header("Location: index.php");
+}
+
+require_once("../config.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Indication &middot; Upgrade</title>
+<meta name="robots" content="noindex, nofollow">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="../resources/bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+<style type="text/css">
+body {
+    padding-top: 60px;
+}
+</style>
+<link href="../resources/bootstrap/css/bootstrap-responsive.min.css" type="text/css" rel="stylesheet">
+<!-- Javascript start -->
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<script src="../resources/jquery.min.js"></script>
+<script src="../resources/bootstrap/js/bootstrap.min.js"></script>
+<!-- Javascript end -->
+</head>
+<body>
+<!-- Nav start -->
+<div class="navbar navbar-fixed-top">
+<div class="navbar-inner">
+<div class="container">
+<a class="brand" href="#">Indication</a>
+</div>
+</div>
+</div>
+<!-- Nav end -->
+<!-- Content start -->
+<div class="container">
+<div class="page-header">
+<h1>Upgrade</h1>
+</div>
+<?php
+
+//Version
+$version = "4.4";
+
+if ($version == VERSION) {
+    die("<div class=\"alert alert-info\"><h4 class=\"alert-heading\">Upgrade Notice</h4><p>Indication does not require an upgrade.<p><a href=\"../login.php\" class=\"btn btn-info\">Go To Login</a></p></div></div></body></html>");
+    
+}
+
+$dbhost = DB_HOST;
+$dbuser = DB_USER;
+$dbpassword = DB_PASSWORD;
+$dbname = DB_NAME;
+$adminuser = ADMIN_USER;
+
+//$adminpassword = ADMIN_PASSWORD;
+//$salt = SALT;
+
+//Salt and hash passwords
+//From 4.3.1 --> 4.4
+$temppassword = substr(str_shuffle("abcdefghijklmnopqrstuvwxyz123456789"), 0, 6);
+$randsalt = md5(uniqid(rand(), true));
+$salt = substr($randsalt, 0, 3);
+$hashedpassword = hash("sha256", $temppassword);
+$adminpassword = hash("sha256", $salt . $hashedpassword);
+$uniquekey = UNIQUE_KEY;
+$website = WEBSITE;
+$pathtoscript = PATH_TO_SCRIPT;
+$adcode = AD_CODE;
+$countuniqueonlystate = COUNT_UNIQUE_ONLY_STATE;
+$countuniqueonlytime = COUNT_UNIQUE_ONLY_TIME;
+$ignoreadminstate = IGNORE_ADMIN_STATE;
+$theme = THEME;
+
+$updatestring = "<?php
+
+//Database Settings
+define('DB_HOST', " . var_export($dbhost, true) . ");
+define('DB_USER', " . var_export($dbuser, true) . ");
+define('DB_PASSWORD', " . var_export($dbpassword, true) . ");
+define('DB_NAME', " . var_export($dbname, true) . ");
+
+//Admin Details
+define('ADMIN_USER', " . var_export($adminuser, true) . ");
+define('ADMIN_PASSWORD', " . var_export($adminpassword, true) . ");
+define('SALT', " . var_export($salt, true) . ");
+
+//Other Settings
+define('UNIQUE_KEY', " . var_export($uniquekey, true) . ");
+define('WEBSITE', " . var_export($website, true) . ");
+define('PATH_TO_SCRIPT', " . var_export($pathtoscript, true) . ");
+define('AD_CODE', " . var_export($adcode, true) . ");
+define('COUNT_UNIQUE_ONLY_STATE', " . var_export($countuniqueonlystate, true) . ");
+define('COUNT_UNIQUE_ONLY_TIME', " . var_export($countuniqueonlytime, true) . ");
+define('IGNORE_ADMIN_STATE', " . var_export($ignoreadminstate, true) . ");
+define('THEME', " . var_export($theme, true) . ");
+define('VERSION', " . var_export($version, true) . ");
+
+?>";
+
+//Check if we can connect
+$con = mysql_connect($dbhost, $dbuser, $dbpassword);
+if (!$con) {
+    die("<div class=\"alert alert-error\"><h4 class=\"alert-heading\">Update Failed</h4><p>Error: Could not connect to database (" . mysql_error() . "). Check your database settings are correct.</p><p><a class=\"btn btn-danger\" href=\"javascript:history.go(-1)\">Go Back</a></p></div></div></body></html>");
+}
+
+//Check if database exists
+$does_db_exist = mysql_select_db($dbname, $con);
+if (!$does_db_exist) {
+    die("<div class=\"alert alert-error\"><h4 class=\"alert-heading\">Update Failed</h4><p>Error: Database does not exist (" . mysql_error() . "). Check your database settings are correct.</p><p><a class=\"btn btn-danger\" href=\"javascript:history.go(-1)\">Go Back</a></p></div></div></body></html>");
+}
+
+//Write Config
+$configfile = fopen("../config.php", "w");
+fwrite($configfile, $updatestring);
+fclose($configfile);
+
+mysql_close($con);
+
+?>
+<div class="alert alert-success">
+<h4 class="alert-heading">Upgrade Complete</h4>
+<p>Indication has been successfully upgraded. Because Indication 1.5 uses salt password hashing, your password is now <b><?php echo $temppassword; ?></b>. Please change it to something more memorable as soon as possible using the settings page.<p><a href="../admin/login.php" class="btn btn-success">Go To Login</a></p>
+</div>
+</div>
+<!-- Content end -->
+</body>
+</html>
